93f4b9e9b32e0bcc8db06e143ab30af6431af250
palseums <57275214+palseums@users.noreply.github.com>
2022-08-23 13:57:56 -0500

    Initial commit

---
+++ AppConfigs.java
@@ -0,0 +1,9 @@
+package guru.learningjournal.kafka.examples;
+
+class AppConfigs {
+    final static String applicationID = "FirstProducer123";
+    final static String bootstrapServers = "ec2-52-86-93-19.compute-1.amazonaws.com:9092,ec2-52-86-93-19.compute-1.amazonaws.com:9093";
+    final static String topicName = "producer-test";
+    final static int numEvents = 1000000;
+    static String palaniappan = "palani";
+}
